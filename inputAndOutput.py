5813ac7930bd5df12e1a9bd25ea9eb55c938ba8b
J S <zdrjson@gmail.com>
2016-12-23 14:16:27 +0800

    add inputAndOutput.py

---
+++ inputAndOutput.py
@@ -0,0 +1,36 @@
+print('100 + 200 = ', 100 + 200)
+name = input('please enter your name:')
+print('hello,',name)
+
+a = 100
+if a >= 0:
+    print(a)
+else:
+    print(-a)	
+
+print('I\'m \"OK\"!')
+
+print('I\'m learning\nPython.')
+print('\\\t\\')
+print(r'\\\t\\')
+print(r'''line1
+line2
+line3''')
+
+age = 19
+
+if age >= 18:
+	print('adult')
+else:
+	print('teenager')	
+	
+	
+a = 123
+print(a)	
+a = 'abc'
+print(a)
+
+print(10 /  3)
+print(10 // 3)
+print(10 % 3)
+	
