39beeada8be9e7647f7d4a84689443b11fdbe8ad
J S <zdrjson@gmail.com>
2016-12-26 10:56:47 +0800

    practise for and while of python

--- helloPython3.py
+++ helloPython3.py
@@ -187,16 +187,52 @@ if x:
 	print('True')
 	
 	
-s = input('birth: ')	
-birth = int(s)
-if birth < 2000:
-	print('00前')
-else:
-	print('00后')
+#s = input('birth: ')	
+#birth = int(s)
+#if birth < 2000:
+#	print('00前')
+#else:
+#	print('00后')
 	
+	
+	
+	
+names = ['Michale','Bob','Tracy']	
+for name in names:
+	print(name)
+	
+sum = 0
+for x in [1,2,3,4,5,6,7,8,9,10]:
+	sum = sum + x
+print(sum)
 
 
+print(list(range(5)))
 
+sum = 0
+for x in range(101):
+    sum = sum + x
+print(sum)
 
+sum = 0
+n = 99
+while n > 0:
+	sum = sum + n
+	n = n - 2
+print(sum)
+
+L = ['Bart','Lisa','Adam']
+
+for x in L:
+	print(x)
+	
+n = 1
+while <= 100:
+	if n > 10:
+		break
+	print(n)
+	n = n + 1
+print('END')
+	
 
 	
