45b96e6798b2b87a968ad90567f960b31c650aaa
J S <zdrjson@gmail.com>
2016-12-30 17:18:07 +0800

    add python property

--- helloPython3.py
+++ helloPython3.py
@@ -1031,6 +1031,109 @@ def readImage(fp):
 	return None
 
 
+class Student(object):
+	name = 'Student'
+s = Student()
+print(s.name)
+
+s.name = 'Michael'
+print(s.name)
+print(Student.name)
+del s.name
+print(s.name)
+
+class Student:
+    pass
+
+s = Student()
+s.name = 'Michael'
+print(s.name)
+
+
+def set_age(self, age):
+	self.age = age
+from types import MethodType
+s.set_age = MethodType(set_age,s)
+s.set_age(25)
+print(s.age)
+
+def set_score(self, score):
+	self.score = score
+	
+Student.set_score = set_score
+
+s.set_score(100)
+print(s.score)
+s2 = Student()
+s2.set_score(99)
+print(s2.score)
+
+class Student(object):
+      __slots__ = ('name','age')
+s = Student()
+s.name = 'Michael'
+s.age = 25
+#s.score = 99
+
+
+class GraduateStudent(Student):
+	pass
+	
+g = GraduateStudent()
+g.score = 99
+print(g.score)
+
+
+class Student(object):
+	def get_score(self):
+		return self._score
+	def set_score(self, value):
+		if not isinstance(value, int):
+			raise ValueError('score must be an integer!')
+		if value < 0 or value > 100:
+			raise ValueError('score must between 0 ~ 100!')
+		self._score = value
+
+s = Student()
+s.set_score(60)
+print(s.get_score())
+
+
+
+
+
+class Student(object):
+	@property
+	def score(self):
+		return self._score
+	@score.setter
+	def score(self, value):
+		if not isinstance(value, int):
+			raise ValueError('score must be an integer!')
+		if value < 0 or value > 100:
+			raise ValueError('score must between 0 ~ 100!')
+		self._score = value
+		
+s = Student()
+s.score = 60
+print(s.score)
+
+
+class Student(object):
+	
+	@property
+	def birth(self):
+		return self._birth 
+		
+	@birth.setter
+	def birth(self, value):
+		self._birth = value
+	
+	@property
+	def age(self):
+		return 2015 - set.birth
+		
+
 
 		
 	
