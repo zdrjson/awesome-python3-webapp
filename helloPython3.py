43ef91fc71f65e071355958f420a92a2506fd058
J S <zdrjson@gmail.com>
2016-12-31 10:44:44 +0800

    python 多继承

--- helloPython3.py
+++ helloPython3.py
@@ -1134,6 +1134,62 @@ class Student(object):
 		return 2015 - set.birth
 		
 
+class Animal(object):
+	pass
+
+class Mammal(Animal):
+	pass
+
+class Bird(Animal):
+	pass
+
+class Dog(Mammal):
+	pass
+	
+class Bat(Mammal):
+	pass
+
+class Parrot(Bird):
+	pass
+	
+class Ostrich(Bird):
+	pass
+	
+class Runnable(object):
+	def run(self):
+		print('Running...')
+	
+class Flyable(object):
+	def fly(self):
+		print('Flying...')
+		
+class Dog(Mammal, Runnable):
+	pass
+	
+class Bat(Mammal, Flyable):
+	pass
+	
+	
+	
+	
+#class MyTCPServer(TCPServer, ForkingMixIn):
+#	pass
+
+
+class Student(object):
+	def __init__(self,name):
+		self.name = name
+	def __str__(self):
+		return 'Student object (name: %s)' % self.name
+print(Student('Michael'))
+
+
+		
+		
+	
+
+
+	
 
 		
 	
